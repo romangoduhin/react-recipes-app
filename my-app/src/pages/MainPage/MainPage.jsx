dad46ac8818635bfe06935b1d35233c91ac91918
Raman <superchewits2000@gmail.com>
2022-11-10 12:23:04 +0300

    feat: add api logic, add recipes list to main page

--- my-app/src/pages/MainPage/MainPage.jsx
+++ my-app/src/pages/MainPage/MainPage.jsx
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import styles from './MainPage.module.scss';
+import spoonacularAPI from "../../services/spoonacularApi/api"
 
 function MainPage() {
+    const [recipes, setRecipes] = useState([]);
+
+    async function setRandomRecipes() {
+        const count = 10;
+        const recipes = await spoonacularAPI.getRandomRecipes(count);
+        setRecipes(recipes);
+    }
+
+    useEffect(() => {
+        setRandomRecipes();
+    }, [])
+
     return (
-        <div className={styles.mainPageWrapper}>Main page</div>
+        <div className={styles.mainPageWrapper}>
+            <ul>{recipes && recipes.map(el => <li key={el.id}>{el.title}</li>)}</ul>
+        </div>
     );
 }
 
