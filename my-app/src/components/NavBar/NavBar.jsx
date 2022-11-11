dcf9cf9a65e8f0f9f48d6d39ffaca4a99862ba65
Raman <superchewits2000@gmail.com>
2022-11-11 19:09:46 +0300

    feat: update NavBar

--- my-app/src/components/NavBar/NavBar.jsx
+++ my-app/src/components/NavBar/NavBar.jsx
@@ -1,10 +1,35 @@
 import React from 'react';
-import style from './NavBar.module.scss'
+import styles from './NavBar.module.scss';
 
 function NavBar() {
     return (
-        <div className={style.navBarWrapper}>
-                NavBar
+        <div className={styles.navBar}>
+            <div className={styles.logoBlock}>
+                <img src="/logoBig.png" alt="logoImage"/>
+            </div>
+
+            <div className={styles.stick}/>
+
+            <div className={styles.searchBlock}>
+                <input
+                    placeholder="Search recipes"
+                    type="text"
+                />
+            </div>
+
+            <div className={styles.profileBlock}>
+                <div className={styles.logo}>
+                    <img src="/userLogo.png" alt="user logo"/>
+                </div>
+
+                <div className={styles.auth}>
+                    <button>Login</button>
+
+                    <span> or </span>
+
+                    <button>Sign up</button>
+                </div>
+            </div>
         </div>
     );
 }
