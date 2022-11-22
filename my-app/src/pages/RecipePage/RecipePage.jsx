a142e6801273fe058f49e6d1f0c49c3f80ec72ec
Raman <superchewits2000@gmail.com>
2022-11-22 16:55:38 +0300

    feat: update RecipePage, add new mixin

--- my-app/src/pages/RecipePage/RecipePage.jsx
+++ my-app/src/pages/RecipePage/RecipePage.jsx
@@ -5,6 +5,7 @@ import Loading from "../../components/Loading/Loading";
 import styles from "./RecipePage.module.scss";
 import ButtonWithIcon from "../../components/ButtonWithIcon/ButtonWithIcon";
 import DifficultyIndicator from "./components/DifficultyIndicator/DifficultyIndicator";
+import parse from "html-react-parser";
 
 
 function RecipePage() {
@@ -14,6 +15,45 @@ function RecipePage() {
 
     function getRandDiffLvl(min, max) {
         return Math.floor(min + Math.random() * (max + 1 - min)); //API doesnt have diff level, here we simulate it
+    }
+
+    function getIngredients(ingredients) {
+        return ingredients.map((el) => {
+            return <li key={el.id}>
+                <span>
+                    {el.measures.metric.amount}{el.measures.metric.unitShort}&nbsp;
+                </span>
+                {el.name}
+            </li>
+        })
+    }
+
+    function getParsedHtml(string) {
+        return parse(string);
+    }
+
+    function getSteps(steps) {
+        const stepsCount = steps.length - 1;
+
+        return <div className={styles.steps}>
+            {steps.map((step) => {
+                return <div className={styles.step}>
+                    <h4>Step {step.number} of {stepsCount}</h4>
+                    <br/>
+                    <span>{step.step}</span>
+                </div>
+            })}
+        </div>
+    }
+
+    function getCategories(categories) {
+        return <div className={styles.list}>
+            {categories.map((category) => {
+                return <div className={styles.category}>
+                    <span>{category}</span>
+                </div>
+            })}
+        </div>
     }
 
     async function setCurrentRecipeData(id) {
@@ -69,6 +109,50 @@ function RecipePage() {
 
                     <div>
                         <ButtonWithIcon src={'/favouriteIcon.png'}/>
+                    </div>
+                </div>
+
+                <div className={styles.content}>
+                    <div className={styles.ingrAndCateg}>
+                        <div className={styles.ingredients}>
+                            <div className={styles.header}>
+                                <h2>Ingredients</h2>
+                                <br/>
+                                <h4>Number of servings: {currentRecipe.servings}</h4>
+                            </div>
+
+                            <ul className={styles.list}>
+                                {getIngredients(currentRecipe.extendedIngredients)}
+                            </ul>
+                        </div>
+                        <br/>
+                        <div className={styles.categories}>
+                            <h2>Ingredients</h2>
+                            <br/>
+                            {getCategories(currentRecipe.dishTypes)}
+                            <br/>
+                            <span className={styles.extraInfo}>
+                                These categories are provided as a guide only and do
+                                reflect any ingredients or products displayed or
+                                referred to on this recipe page and that are not
+                                listed in the recipe’s ingredients list.
+                                Find out more about allergy, dietary and lifestyle category tags.
+                            </span>
+                        </div>
+                    </div>
+
+                    <div className={styles.description}>
+                        <div className={styles.summary}>
+                            <h2>Description</h2>
+                            <br/>
+                            <span>{getParsedHtml(currentRecipe.summary)}</span>
+                        </div>
+                        <br/>
+                        <div className={styles.method}>
+                            <h2>Method</h2>
+                            <br/>
+                            {getSteps(currentRecipe.analyzedInstructions[0].steps)}
+                        </div>
                     </div>
                 </div>
             </div>
